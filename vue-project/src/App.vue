94881b4ea0e1d933dd6c1d9d532c764989729e20
Guillaume-Linez <guillaumelinez1@gmail.com>
2023-12-14 15:42:45 +0100

    fix log

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <button @click="fetchData">Récupérer des données de la base de données</button>
     <ul>
-      <li v-for="data in databaseData" :key="data._id">{{ data.field }}</li>
+      <li v-for="data in databaseData">{{ data.REFERENCE }} {{ data.CURRENCY }} {{ data.PRICE }} {{ data.COUNTRY }}</li>
     </ul>
   </div>
 </template>
@@ -20,11 +20,11 @@ export default {
     fetchData() {
       axios.get('http://localhost:3000/api/data') // Assurez-vous que l'URL correspond à votre backend
         .then(response => {
-          // this.databaseData = response.data;
+          this.databaseData = response.data;
           console.log(response.data)
-          response.data.forEach(e => {
-            console.log(e)
-          });
+          // response.data.forEach(e => {
+          //   // console.log(e)
+          // });
         })
         .catch(error => {
           console.error('Erreur lors de la récupération des données', error);
